04992d9da0b85b08b2f4185efec2cfd217fb9093
Aline Prado Leporic <aline.leporic@unifesp.br>
2025-09-08 17:04:12 -0300

    180 A .c

---
+++ 1180 A .c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int N;
+    
+    scanf("%d", &N);
+    
+    int x[N];
+    int i;
+    
+    for(i=0; i<N; i++){
+        scanf("%d", &x[i]);
+    }
+    
+    int menor = x[0];
+    int posicao= 0;
+    
+    for(i=0; i<N; i++){
+        if(x[i]< menor){
+            menor = x[i];
+            posicao = i;
+        }
+    }
+    printf("Menor valor: %d\n", menor);
+    printf("Posicao: %d\n", posicao);
+ 
+    return 0;
+}
