73e328e8e7db031e7bfb5d61914d747109b21b86
Aline Prado Leporic <aline.leporic@unifesp.br>
2025-11-26 15:35:18 -0300

    1132.c

---
+++ 1132.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int X = 0;
+    int Y = 0;
+    int i, soma=0;
+
+    scanf("%d",&X);
+    scanf("%d",&Y);
+
+    if(X < Y){
+        for(i=X; i<=Y; i++){
+            if(i % 13!=0){
+                soma+=i;
+            }
+
+        }
+    }
+    else{
+         for(i=Y; i<=X; i++){
+            if(i % 13!=0){
+                soma+=i;
+            }
+
+        }
+    }
+
+    printf("%d\n", soma);
+
+
+    return 0;
+}
